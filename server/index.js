4991ec291e4f5e6f53c3ac6a7351ccfa9dd78bf4
Rafaelksx <rr490233@gmail.com>
2025-12-12 00:09:30 -0400

    Fix: Configurar CORS y URL de api para produccion

--- server/index.js
+++ server/index.js
@@ -44,7 +44,11 @@ const upload = multer({ storage: storage });
 // Conectar a la Base de Datos
 connectDB();
 
-app.use(cors());
+app.use(cors({
+    origin: '*', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 
 // --- RUTAS API ---
